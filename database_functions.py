68f6ad37ab1015249b868fb03421a15d5dafdcc2
Matthew Gleich <matthewgleich@gmail.com>
2019-06-30 22:32:04 -0400

    Update database_functions.py

--- database_functions.py
+++ database_functions.py
@@ -16,6 +16,34 @@ def init_field_information_section(list_of_field_names):
 
     # Firebase interactions:
     cred = credentials.Certificate("firestore_creds.json")
-    firebase_admin.initialize_app(cred, {"databaseURL": "https://ghs-app-5a0ba.firebaseio.com"})
+    firebase_admin.initialize_app(cred, {"databaseURL": "https://ghs-app-5a0ba.firebaseio.com/"})
     ref = db.refernce("field_information")
-    general_information_path = 
+    child_ref = ref.child("general-information")
+    child_ref.set({
+        "events-so-far": 0,
+        "total-events-today": 0,
+        "total-events-today-list": [],
+        "active-events": 0,
+        "active-events-list": []
+    })
+    for field in list_of_field_names:
+        field_ref = ref.child("field-status/" + field)
+        field_ref.set({
+            "last-update": str(datetime.now()),
+            "sport": "",
+            "start-time": "00:00:00",
+            "end-time": "00:00:00",
+            "away-team-name": "",
+            "varsity-sport": False
+        })
+    print("created/reset field_information section in realtime database")
+
+
+# Testing:
+init_field_information_section([
+    "softball-field",
+    "baseball-field",
+    "gym",
+    "football-field",
+    "jv-field"
+])
