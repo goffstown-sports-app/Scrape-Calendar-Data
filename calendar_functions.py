3d97077fe4f86a79b544f8a49e20a0497050d344
Matthew Gleich <43759105+Matt-Gleich@users.noreply.github.com>
2019-07-01 10:00:53 -0700

    Create cleaning_response function

--- calendar_functions.py
+++ calendar_functions.py
@@ -48,4 +48,63 @@ def get_events_for_day(day, month, year):
 # print(get_events_for_day(6, 6, 2019))
 
 
-        
+def cleaning_response(json_data):
+    """
+    Will clean the json response from the website.
+    :param json_data: json response from the calendarAjax file
+    :return: cleaned data (list)
+    """
+    # Type Checking:
+    UF.check_type(json_data, "list")
+    
+    #  Cleaning Data:
+    if len(json_data) == 0:
+        return None
+    else:
+        event_amount = 0
+        clean_events = []
+        for event in json_data:
+            event_number += 1
+            if event["isCancelled"] == 0:
+                cancelled = False
+            elif event["isCancelled"] == 1:
+                cancelled = True
+            if "boys" in event["theTitle"].lower():
+                gender = "m"
+            elif "girls" in event["theTitle"].lower():
+                gender = "g"
+            if "varsity" in event["theTitle"].lower():
+                varsity = True
+            elif "junior varsity" in event["theTitle"].lower():
+                varsity = False
+            if "middle" in event["theTitle"].lower():
+                ghs_sport = False
+            elif "middle" not in event["theTitle"].lower():
+                ghs_sport = True
+            if event["homeOrAway"] == 1:
+                home = True
+            elif event["homeOrAway"] == 0:
+                home = False
+            location = event["thePlace"].strip("@").strip()
+            thedate_elements = event["thedate"].split("vs")
+            normal_time = thedate_elements[0].strip("(H)").strip("(A)").strip()
+            datetime_from = UF.normal_time_to_datetime(normal_time, day, month, year)
+            hour = datetime_from.hour
+            minute = datetime_form.minute
+            away_team_name = thedate_elements[1].strip()
+            sport_name = event["theTitle"].strip("Boys").strip("Girls").strip("Junior").strip("Varsity").strip("Middle").strip()
+            event_dict = {
+                "gender": gender,
+                "varsity": varsity,
+                "ghs_sport": ghs_sport,
+                "home": home,
+                "event_location": location,
+                "start-time-(nomral)": normal_time,
+                "stat-time-(datetime)": datetime_form,
+                "hour": hour,  # Military time
+                "minute": minute, # Military time
+                "away_team_name": away_team_name,
+                "sport": sport_name,
+            }
+            clean_event.append(event_dict)
+        return clean_event
