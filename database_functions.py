bf7c91cd9d08a366e162e011eb419fb89f360537
Matthew Gleich <matthewgleich@gmail.com>
2019-06-30 20:33:11 -0400

    Create init_field_information_section function

--- database_functions.py
+++ database_functions.py
@@ -3,55 +3,19 @@ from firebase_admin import credentials
 from firebase_admin import db
 import utility_functions as UF
 from datetime import datetime
-import json
 
 
-def upload_day_events(home, sport_name, cancelled, postponed, event_time, away_team_name, location, event_name, event_number):
+def init_field_information_section(list_of_field_names):
     """
-    Uploads all the events for the day to the Realtime database
-    :param home: event is home (bool)
-    :param sport_name: name of the sport (str)
-    :param cancelled: if event is cancelled (bool)
-    :param postponed: if event is postponed (bool)
-    :param event_time: time of event (str)
-    :param away_team_name: name of the away team (str)
-    :param location: name of the location (str)
-    :param event_name: name of the event (str)
-    :param event_number: number of event (int)
+    Initializes the field_information section of the realtime database
+    :param list_of_field_names: list of all the field names
     :return: none
     """
-    # Type Checking:
-    UF.check_type(home, "bool")
-    UF.check_type(sport_name, "str")
-    UF.check_type(cancelled, "bool")
-    UF.check_type(postponed, "bool")
-    UF.check_type(event_time, "str")
-    UF.check_type(away_team_name, "str")
-    UF.check_type(location, "str")
-    UF.check_type(event_name, "str")
-    UF.check_type(event_number, "int")
+    # Type checking:
+    UF.check_type(list_of_field_names, "list")
 
-    # Data manipulation:
-    datetime_now = datetime.now()
-    current_year = datetime_now.year
-    current_month = datetime_now.month
-    current_day = datetime_now.day
-
-    # Realtime Database interactions:
+    # Firebase interactions:
     cred = credentials.Certificate("firestore_creds.json")
     firebase_admin.initialize_app(cred, {"databaseURL": "https://ghs-app-5a0ba.firebaseio.com"})
-    ref = db.reference("events")
-    child_ref = ref.child("tday_events/event_" + str(event_number))
-    child_ref.set({
-        "current-year": current_year,
-        "current-month": current_month,
-        "current-day": current_day,
-        "home": home,
-        "sport-name": sport_name,
-        "cancelled": cancelled,
-        "postponed": postponed,
-        "event_time": event_time,
-        "away_team_name": away_team_name,
-        "location": location,
-        "event_name": event_name
-    })
+    ref = db.refernce("field_information")
+    general_information_path = 
