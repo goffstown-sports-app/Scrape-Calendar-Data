050e37a868d453e38b04466960daa9f813eeada7
Matthew Gleich <43759105+Matt-Gleich@users.noreply.github.com>
2019-07-23 18:08:27 -0400

    Test for Travis CI

--- calendar_functions.py
+++ calendar_functions.py
@@ -23,22 +23,7 @@ def get_events_for_day(day, month, year):
         'origin': 'https://goffstownathletics.com',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9',
-        'x-requested-with': 'XMLHttpRequest',
-        'cookie': 'JSESSIONID=A437B190923E2C2C1845AF70D5346A7D.Main; erd=6B749E34B06E77B4303C2CF244678ECD; CALVIEW=month; CALDATE={m}/{d}/{y}; AWSALB=e/KIOyE3YhuKL9TvYnaun6DDc9LkzOc+G7+DLvkNdZ9KMu70lUN9dcxt0g5OwJoBLTJgglZ3Tk91OhPU7KZXRz9GUslWJ60RiMNq+6YgwEEwnvdQIGNPVfNKisoh'.format(
-            m=month, d=day, y=year),
-        'pragma': 'no-cache',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'accept': 'application/json, text/javascript, */*; q=0.01',
-        'cache-control': 'no-cache',
-        'authority': 'goffstownathletics.com',
-        'referer': 'https://goffstownathletics.com/main/calendar/',
-    }
-    s = Session()
-    req = Request('POST', url, data=data, headers=headers)
-    prepped = s.prepare_request(req)
-    resp = s.send(prepped)
-    resp_json = json.loads(resp.content.decode("utf-8"))
+        'x-requeste
     if len(resp_json) == 0:
         return None
     return resp_json
