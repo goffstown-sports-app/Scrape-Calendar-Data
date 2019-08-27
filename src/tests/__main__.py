f69ad989be8108bf0296de3fbbe6eaea0fdfebe6
Matthew Gleich <matthewgleich@gmail.com>
2019-08-27 01:53:57 -0400

    Create __main__.py

---
+++ src/tests/__main__.py
@@ -0,0 +1,6 @@
+from os import system
+
+system("python test-calendar_actions.py")
+system("python test-database.py")
+system("python test-utils-datetime_utils.py")
+system("python test-utils-generic.py")
