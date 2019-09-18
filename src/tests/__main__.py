fbf5630e019e2e960333b9f30805e6aae912b82d
Matthew Gleich <43759105+Matt-Gleich@users.noreply.github.com>
2019-09-18 09:52:59 -0400

    Delete __main__.py

--- src/tests/__main__.py
+++
@@ -1,6 +0,0 @@
-from os import system
-
-system("python test-calendar_actions.py")
-system("python test-database.py")
-system("python test-utils-datetime_utils.py")
-system("python test-utils-generic.py")
