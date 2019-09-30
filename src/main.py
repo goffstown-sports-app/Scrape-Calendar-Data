e5ce2b3e84e7131d805e0fff15a0f6a5986a1751
Matthew Gleich <matthewgleich@gmail.com>
2019-09-30 16:41:40 -0400

    Use firebase_creds.json inside /src/secrets folder

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def main():
     Runs main
     :return: none
     """
-    cred = credentials.Certificate("firestore_creds.json")
+    cred = credentials.Certificate("./secrets/firestore_creds.json")
     firebase_admin.initialize_app(
         cred, {
             "databaseURL": "https://ghs-app-5a0ba.firebaseio.com/",
