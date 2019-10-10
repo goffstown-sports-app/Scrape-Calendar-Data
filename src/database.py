dbba74039fe8140afdc7a4804186488e271aad48
Matthew Gleich <matthewgleich@gmail.com>
2019-10-10 17:47:35 -0400

    Remove / from child

--- src/database.py
+++ src/database.py
@@ -59,7 +59,7 @@ def update_status_database(cleaned_data, current_hour):
                 got_data += 1
             if got_data != 0:
                 ref = db.reference("field-information")
-                child_ref = ref.child("/field-status/" + field_name)
+                child_ref = ref.child("field-status/" + field_name)
                 current_ref = child_ref.get()
                 try: 
                     if current_ref["home-score"] != type(0) or current_ref["away-score"] != type(0):
